e176eb2cc6744d9358ae668a7c1392ded1a2b26d
Juneyong <juneyong87@gmail.com>
2024-12-04 15:59:22 +0900

    [Silver III] Title: 파스칼, Time: 92 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/2986. 파스칼/파스칼.py
+++ 백준/Silver/2986. 파스칼/파스칼.py
@@ -4,21 +4,18 @@ input = sys.stdin.readline
 
 n = int(input())
 
-if n == 1:
-    print(0)
-else:
-    measure = []
+measure = []
 
-    for k in range(1, int(sqrt(n))+1):
-        if n % k == 0:
-            measure.append(k)
+for k in range(1, int(sqrt(n))+1):
+    if n % k == 0:
+        measure.append(k)
 
-    x = [] + measure
+x = [] + measure
 
-    for k in measure:
-        if n%k == 0:
-            x.append(n//k)
+for k in measure:
+    if n%k == 0:
+        x.append(n//k)
 
-    x.sort()
+x.sort()
 
-    print(n - x[-2])
+print(n - x[-2])
